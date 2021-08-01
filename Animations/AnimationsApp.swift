1ac06f274d219ea8274bc78c393a953255c123ba
Rachelle Lee <rachedeveloper@gmail.com>
2021-08-01 14:47:21 -0700

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by RachelleLee on 8/1/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
